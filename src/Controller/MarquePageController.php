e4f08cf02e02afd75ad58990ae9cd1319e6fc925
Prothin Camille <camille.prothin@gmail.com>
2024-02-15 08:32:17 +1100

    ex 1

---
+++ src/Controller/MarquePageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\MarquePage;
+use Doctrine\ORM\EntityManagerInterface;
+
+class MarquePageController extends AbstractController
+{
+    #[Route('/marque/page', name: 'app_marque_page')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $marque_pages = $entityManager->getRepository(MarquePage::class)->findAll();
+        return $this->render('marque_page/index.html.twig', [
+            'controller_name' => 'MarquePageController',
+            'marque_page' => $marque_pages,
+        ]);
+    }
+}
