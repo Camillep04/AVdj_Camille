1a3ed9189b1dd6030de1e75a787e9c5a3e81e701
Prothin Camille <camille.prothin@gmail.com>
2024-02-15 11:47:47 +1100

    ex 3

--- src/Controller/MarquePageController.php
+++ src/Controller/MarquePageController.php
@@ -9,28 +9,46 @@ use App\Entity\MarquePage;
 use Doctrine\ORM\EntityManagerInterface;
 
 
-class MarquePageController extends AbstractController
-{
-    #[Route('/marque/page', name: 'app_marque_page')]
-    public function index(EntityManagerInterface $entityManager): Response
-    {
-        $marque_pages = $entityManager->getRepository(MarquePage::class)->findAll();
-        return $this->render('marque_page/index.html.twig', [
-            'controller_name' => 'MarquePageController',
-            'marque_page' => $marque_pages,
-        ]);
-    }
-    #[Route("/marquepage/ajouter", name: "marquepage_ajouter")]
-    public function ajouterMarquePage(EntityManagerInterface $entityManager): Response
-    {
-        $marquepage = new MarquePage();
-        $marquepage->setUrl("https://www.meteo.nc/");
 
-        $marquepage->setDateCreation (new \DateTime());
-        $marquepage->setCommentaire("ouioui il pleut");
-        $entityManager->persist($marquepage);
-        $entityManager->flush();
-        return new Response("Marque page sauvegardé avec l'id ". $marquepage->getId());
-    }
+    #[Route("/marque/page", requirements: ["_locale" => "en|es|fr"], name: "app_marque_page_")]
+    class MarquePageController extends AbstractController
+    {
 
-}
+        #[Route('/', name: 'app_marque_page')]
+        public function index(EntityManagerInterface $entityManager): Response
+        {
+            $marque_pages = $entityManager->getRepository(MarquePage::class)->findAll();
+            return $this->render('marque_page/index.html.twig', [
+                'controller_name' => 'MarquePageController',
+                'marque_page' => $marque_pages,
+            ]);
+        }
+        #[Route("/marquepage/ajouter", name: "marquepage_ajouter")]
+        public function ajouterMarquePage(EntityManagerInterface $entityManager): Response
+        {
+            $marquepage = new MarquePage();
+            $marquepage->setUrl("https://www.meteo.nc/");
+
+            $marquepage->setDateCreation (new \DateTime());
+            $marquepage->setCommentaire("ouioui il pleut");
+            $entityManager->persist($marquepage);
+            $entityManager->flush();
+            return new Response("Marque page sauvegardé avec l'id ". $marquepage->getId());
+        }
+        #[Route("/details/{id<\d+>}", name: "details")]
+        public function afficherDetails(int $id, EntityManagerInterface $entityManager): Response
+        {
+            $details = $entityManager
+            ->getRepository(MarquePage::class)
+            ->find($id);
+            if (!$details) {
+                throw $this->createNotFoundException(
+                "Aucun details avec l'id ". $id
+                );
+            }
+            
+            return $this->render('marque_page/details.html.twig', [
+                'details' => $details,
+            ]);
+        } 
+    }  
