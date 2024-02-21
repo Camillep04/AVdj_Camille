4416520ac6cfda2765f9bc7898e3308f2447e6a3
Prothin Camille <camille.prothin@gmail.com>
2024-02-21 11:39:01 +1100

    ex5 partie 4

--- src/Controller/CaillouController.php
+++ src/Controller/CaillouController.php
@@ -5,14 +5,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Faune;
+use App\Entity\Flore;
+use Doctrine\ORM\EntityManagerInterface;
 
+
+#[Route("/caillou", requirements: ["_locale" => "en|es|fr"], name: "app_caillou_")]
 class CaillouController extends AbstractController
 {
-    #[Route('/caillou', name: 'app_caillou')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('caillou/index.html.twig', [
             'controller_name' => 'CaillouController',
         ]);
     }
+    #[Route("/faune", name: "faune")]
+        public function afficherFaune(EntityManagerInterface $entityManager): Response
+        {
+            $faune = $entityManager
+            ->getRepository(Faune::class)
+            ->findAll();            
+            return $this->render('caillou/faune.html.twig', [
+                'faune' => $faune,
+            ]);
+        } 
+    #[Route("/flore", name: "flore")]
+        public function afficherFlore(EntityManagerInterface $entityManager): Response
+        {
+            $flore = $entityManager
+            ->getRepository(Flore::class)
+            ->findAll();            
+            return $this->render('caillou/flore.html.twig', [
+                'flore' => $flore,
+            ]);
+        } 
 }
---
+++ src/Entity/Faune.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FauneRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FauneRepository::class)]
+class Faune
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $img = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(?string $img): static
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
